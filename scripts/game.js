9d2e2f59deed8a67249f450006c2dc1bc353ce62
Willian Samer de Oliveira <willian.samer@gmail.com>
2021-08-09 15:53:41 -0300

    add botão de reinicio e comando de empate

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,4 @@
-let board = ["", "", "", "", "", "", "", "", ""];
+/* let board = ["", "", "", "", "", "", "", "", ""];
 let player = 0;
 let symbol = ["p1", "p2"];
 let gameOver = false;
@@ -47,4 +47,73 @@ function isWin(){
     }
   }
   return false;
+} */
+
+//________________________Treinamento__________________________________
+
+let board = ["", "", "", "", "", "", "", "", ""];
+let player = 0;
+let symbol = ["p1", "p2"];
+let gameOver = false;
+let filledBoard=[];
+let n = 0;
+
+let btnReiniciar = document.querySelector(".box-restart");
+btnReiniciar.style.display = "none";
+
+function handleStep(position){
+
+  if(gameOver){
+    return;
+  }
+
+  if(board[position] === ""){
+    board[position] = symbol[player];
+    filledBoard.length++
+
+    gameOver = toWin();
+
+    if(!gameOver){
+      if(player == 0){
+        player = 1;
+      } else{
+        player = 0;
+      }
+    }
+  } 
+  check();
+
+  return gameOver;
+}
+
+function toWin(){
+  let check = 
+  [[0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],]
+
+  for(let i = 0; i < check.length; i++){
+    let seq = check[i];
+    let pos1 = seq[0];
+    let pos2 = seq[1];
+    let pos3 = seq[2];
+
+
+    if(board[pos1] === board[pos2] && board[pos2] === board[pos3] && board[pos1] === board[pos3] && board[pos1] != ""){
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function check(){
+  if(filledBoard.length ===9){
+    setTimeout(empate(), 100);
+  }
 }
--- scripts/interface.js
+++ scripts/interface.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", startGame);
+/* document.addEventListener("DOMContentLoaded", startGame);
 
 function startGame(){
   let squares = document.querySelectorAll(".square");
@@ -15,7 +15,7 @@ function handleClick(element){
   if(handleMove(position)){
     setTimeout(()=>{alert(`Fim de Jogo. O jogador  ${symbol[player]}  ganhou`)}, 10);
   }
-  
+
   updateSquare();
 }
 
@@ -31,4 +31,60 @@ function updateSquare(){
       square.innerHTML = `<div class="${filled}"></div>`
     }
   })
+} */
+
+//________________________Treinamento__________________________________
+
+document.addEventListener("DOMContentLoaded", startInterface);
+
+function startInterface(){
+  let squares = document.querySelectorAll(".square");
+  
+  squares.forEach((square)=>{
+    square.addEventListener("click", handleClick);
+  })
+}
+
+function handleClick(event){
+  let position = event.target.id;
+  
+
+  handleStep(position);
+  if(gameOver){
+    setTimeout(
+      restartGame(), 10);
+
+  }
+
+  updateSquare(position);
+}
+
+function updateSquare(position){
+  let squares = document.querySelectorAll(".square");
+
+  if(board[position] != ""){
+    squares[position].innerHTML = `<div class="${board[position]}"></div>`;
+  }
+}
+
+function restartGame(){
+  let h3 = document.querySelector(".ganhador"); 
+
+  h3.innerHTML = `<div class="ganhador">O ganhador foi o jogador ${symbol[player]}</div> <img height="40px" src="./assets/${symbol[player]}.png" alt="">`
+  btnReiniciar.style.display = "flex";
+
+}
+
+function reinicio(){
+  btnReiniciar.style.display = "none";
+  window.location.reload();
+}
+
+function empate(){
+  let h3 = document.querySelector(".ganhador"); 
+
+  h3.innerHTML = "<div class='ganhador'>Empate</div>";
+  btnReiniciar.style.display = "flex";
+
+
 }
